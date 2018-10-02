554201793a73c4d2a3baea624d5268c7f477aeba
Aurelia Smith <smith_aurelia@columbusstate.edu>
2018-10-02 20:41:19 +0000

    Adding terrible error pages

---
+++ feedback.php
@@ -0,0 +1,96 @@
+<?php 
+// This script performs an INSERT query to add a record to the users table.
+
+$page_title = 'Feedback';
+include('../includes/header.html');
+
+// Check for form submission:
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	require('../mysqli_connect.php'); // Connect to the db.
+
+	$errors = []; // Initialize an error array.
+
+	// Check for a first name:
+	if (empty($_POST['first_name'])) {
+		$errors[] = 'You forgot to enter your first name.';
+	} else {
+		$fn = mysqli_real_escape_string($dbc, trim($_POST['first_name']));
+	}
+
+	// Check for a last name:
+	if (empty($_POST['last_name'])) {
+		$errors[] = 'You forgot to enter your last name.';
+	} else {
+		$ln = mysqli_real_escape_string($dbc, trim($_POST['last_name']));
+	}
+
+	// Check for an email address:
+	if (empty($_POST['email'])) {
+		$errors[] = 'You forgot to enter your email address.';
+	} else {
+		$e = mysqli_real_escape_string($dbc, trim($_POST['email']));
+	}
+
+	// Check for feedback:
+	if (!empty($_POST['feedb'])) {
+		$p = mysqli_real_escape_string($dbc, trim($_POST['feedb']));
+	}
+	else {
+		$errors[] = 'You forgot to enter feedback.';
+	}
+
+	if (empty($errors)) { // If everything's OK.
+
+		// Register the user in the database...
+
+		// Make the query:
+		$q = "INSERT INTO users (first_name, last_name, email, pass, registration_date) VALUES ('$fn', '$ln', '$e', SHA2('$p', 512), NOW() )";
+		$r = @mysqli_query($dbc, $q); // Run the query.
+		if ($r) { // If it ran OK.
+
+			// Print a message:
+			echo '<h1>Thank you!</h1>
+		<p>Youe feedback is appreciated!</p><p><br></p>';
+
+		} else { // If it did not run OK.
+
+			// Public message:
+			echo '<h1>System Error</h1>
+			<p class="error">Your feedback sank! We apologize for any inconvenience.</p>';
+
+			// Debugging message:
+			echo '<p>' . mysqli_error($dbc) . '<br><br>Query: ' . $q . '</p>';
+
+		} // End of if ($r) IF.
+
+		mysqli_close($dbc); // Close the database connection.
+
+		// Include the footer and quit the script:
+		include('includes/footer.html');
+		exit();
+
+	} else { // Report the errors.
+
+		echo '<h1>Error!</h1>
+		<p class="error">The following error(s) occurred:<br>';
+		foreach ($errors as $msg) { // Print each error.
+			echo " - $msg<br>\n";
+		}
+		echo '</p><p>Please try again.</p><p><br></p>';
+
+	} // End of if (empty($errors)) IF.
+
+	mysqli_close($dbc); // Close the database connection.
+
+} // End of the main Submit conditional.
+?>
+<h1>Feedback</h1>
+<form action="register.php" method="post">
+	<p>First Name: <input type="text" name="first_name" size="15" maxlength="20" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>"></p>
+	<p>Last Name: <input type="text" name="last_name" size="15" maxlength="40" value="<?php if (isset($_POST['last_name'])) echo $_POST['last_name']; ?>"></p>
+	<p>Email Address: <input type="email" name="email" size="20" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" > </p>
+	<p>Feedback: <input type="textarea" name="feedb" value="<?php if (isset($_POST['feedb'])) echo $_POST['feedb']; ?>" ></p>
+	<p><input type="submit" name="submit" value="Feedback"></p>
+</form>
+<?php include('../includes/footer.html'); ?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$page_title = '';
+include('includes/header.html');
+
+?>
+
+<div class="page-header"><h1></h1></div>
+
+
+<?php
+
+
+include('../includes/footer.html');
+?>
